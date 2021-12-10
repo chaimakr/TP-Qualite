99bb7338a2b7979ae53d2f337fc5d327da9fee87
chaimakr <chaima050@gmail.com>
2021-12-10 16:48:15 +0100

    activite 2 tp 2

--- TP1 Qualité/README.md
+++ TP1 Qualité/README.md
@@ -1,1 +1,2 @@
-# SOLID
+# SOLID 
+#### SOLID is a popular set of design principles that are used in object-oriented software development. SOLID is an acronym that stands for five key design principles: single responsibility principle, open-closed principle, Liskov substitution principle, interface segregation principle, and dependency inversion principle 
--- TP2 Qualité/insat_qa_selenium/src/main/java/HelloSelenium.java
+++ TP2 Qualité/insat_qa_selenium/src/main/java/HelloSelenium.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.List;
 
 import java.time.Duration;
 
@@ -12,17 +13,65 @@ public class HelloSelenium {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        //WebDriverWait waiter = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+
+        // go to tunisianet
         driver.get("https://www.tunisianet.com.tn/");
-        //waiter.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("_desktop_user_info")));
         Thread.sleep(5000);
+
+        // select the authentification icon
         WebElement userButton = driver.findElement(By.cssSelector("#_desktop_user_info > div > div > svg"));
         userButton.click();
-        Thread.sleep(8000);
-        WebElement authentificationButton = driver.findElement(By.cssSelector("#_desktop_user_info > ul > li > a"));
+        Thread.sleep(3000);
+
+        // open the dropdown
+        WebElement authentificationdropdown = driver.findElement(By.cssSelector("#_desktop_user_info > ul > li > a"));
+        authentificationdropdown.click();
+        Thread.sleep(3000);
+
+
+        //click to open the authentification window
+        WebElement authentificationButton = driver.findElement(By.cssSelector(".user-down > li > a "));
         authentificationButton.click();
+        Thread.sleep(3000);
+
+        //fill the authentification form
+        WebElement emailInput = driver.findElement(By.cssSelector(".form-group > div > input"));
+        emailInput.sendKeys("chaima050@gmail.com");
+        Thread.sleep(3000);
+
+        WebElement passwordInput = driver.findElement(By.cssSelector(".form-group > div > div > input"));
+        passwordInput.sendKeys("Selenium");
+        Thread.sleep(3000);
+
+        // sign in
+        WebElement submitButton = driver.findElement(By.id("submit-login"));
+        submitButton.click();
+        Thread.sleep(3000);
+
+        // search product
+        WebElement searchInput = driver.findElement(By.className("search_query"));
+        searchInput.sendKeys("laptop PC portable MacBook M1 13.3 ");
+        Thread.sleep(1000);
+
+        WebElement searchButton = driver.findElement(By.cssSelector("#sp-btn-search > button"));
+        searchButton.click();
+        Thread.sleep(1000);
+
+        List<WebElement> productsTitleList = driver.findElements(By.className("product-title"));
+        productsTitleList.get(0).click();
+        Thread.sleep(2000);
+
+        // finish the purchase process
+        WebElement addProductButton = driver.findElement(By.className("add-to-cart"));
+        addProductButton.click();
+        Thread.sleep(1000);
+
+        WebElement orderButton = driver.findElement(By.cssSelector("a.btn-block"));
+        orderButton.click();
+        Thread.sleep(1000);
+
     }
 }
